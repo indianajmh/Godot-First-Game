280c04f72fdfc3939005c8ce66f4a1c7587bfdf9
indianajmh <indianajmh@gmail.com>
2026-03-18 16:22:09 -0700

    Controller/Score HUD
    Controller support and basic HUD for showing score.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,7 +3,9 @@ extends Node
 var score = 0
 
 @onready var score_label: Label = $ScoreLabel
+@onready var score_hud: Label = $"../Player/Camera2D/ScoreHUD"
 
 func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins"
+	score_hud.text = "Coins: " + str(score)
